05b7fc14e13348390714485b4fc11d0098e17e87
vencislav <v.verchov@mail.bg>
2019-09-21 13:56:55 +0300

    refactoring

--- DB Fundamentals/Basic CRUD/SQLQuery 22.-24.Geography Database .sql
+++ DB Fundamentals/Basic CRUD/SQLQuery 22.-24.Geography Database .sql
@@ -10,7 +10,10 @@ WHERE ContinentCode = 'EU'
 ORDER BY Population DESC, CountryName
 
 --PROBLEM 24
-SELECT CountryName, CountryCode, Currency
-FROM Countries 
-WHERE ContinentCode = 'EU'
-ORDER BY Population DESC, CountryName
+SELECT CountryName, CountryCode,
+CASE
+WHEN CurrencyCode = 'EUR' THEN 'Euro'
+ELSE 'Not Euro'
+END AS Currency
+FROM Countries
+ORDER BY CountryName
