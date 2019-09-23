84a3a92785370021c38809ae8b283b899a719c17
vencislav <v.verchov@mail.bg>
2019-09-23 14:27:07 +0300

    add SQLQuery

--- DB Fundamentals/Basic CRUD/SQLQuery 02.-21.DataBase SoftUni.sql
+++ DB Fundamentals/Basic CRUD/SQLQuery 02.-21.DataBase SoftUni.sql
@@ -12,3 +12,20 @@ FROM Departments
 SELECT FirstName, LastName , Salary
 FROM Employees
 
+--PROBLEM 05
+SELECT FirstName,MiddleName, LastName 
+FROM Employees
+
+--PROBLEM 06
+SELECT FirstName+'.'+LastName+'@softuni.bg' AS 'Full Email Address'
+FROM Employees
+
+--PROBLEM 07
+SELECT DISTINCT Salary
+FROM Employees
+ORDER BY Salary 
+
+--PROBLEM 08
+SELECT *
+FROM Employees
+WHERE  JobTitle = 'Sales Representative';
