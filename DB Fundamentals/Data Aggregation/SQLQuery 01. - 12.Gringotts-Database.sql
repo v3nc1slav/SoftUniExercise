e78176c110da32cd21d7ea7a5992fdf9650ce75d
vencislav <v.verchov@mail.bg>
2019-10-12 23:49:09 +0300

    add sqlquery

--- DB Fundamentals/Data Aggregation/SQLQuery 01. - 12.Gringotts-Database.sql
+++ DB Fundamentals/Data Aggregation/SQLQuery 01. - 12.Gringotts-Database.sql
@@ -21,3 +21,32 @@ FROM WizzardDeposits
 GROUP BY DepositGroup 
 ORDER BY DepositGroup DESC) AS MY
 ORDER BY DepositGroup
+
+--RROBLEM 05
+SELECT DepositGroup,
+       SUM(DepositAmount) AS [TotalSum]
+FROM WizzardDeposits
+GROUP BY DepositGroup; 
+
+--PROBLEM 06
+SELECT DepositGroup,
+       SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup; 
+
+--PROBLEM 07
+SELECT DepositGroup,
+       SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+WHERE (MagicWandCreator = 'Ollivander family') 
+GROUP BY DepositGroup 
+HAVING SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
+--PROBLEM 08
+SELECT DepositGroup, MagicWandCreator, 
+		MIN (DepositCharge) AS MinDepositCharge
+FROM WizzardDeposits
+GROUP BY DepositGroup , MagicWandCreator
+
