24731f85b04ca927a67e13dfcadc146fe5d2a934
vencislav <v.verchov@mail.bg>
2019-10-22 19:27:01 +0300

    add SQLQuery

--- DB Fundamentals/Database Programmability and Transactions/SQLQuery1.sql
+++ DB Fundamentals/Database Programmability and Transactions/SQLQuery1.sql
@@ -57,3 +57,118 @@ BEGIN
 END;
 
 --EXEC usp_GetEmployeesFromTown  SOFIA
+
+--PROBLEM 05
+CREATE or alter  FUNCTION ufn_GetSalaryLevel(@salary INT)
+RETURNS  NVARCHAR(10) 
+BEGIN
+DECLARE @salaryLevel NVARCHAR(10)
+	IF (@salary < 30000)
+	SET @salaryLevel = 'Low'
+	ELSE IF @salary <= 50000
+	SET @salaryLevel = 'Average'
+	ELSE
+	SET @salaryLevel = 'High'
+	RETURN @salaryLevel
+END;
+
+--SELECT e.Salary, DBO.ufn_GetSalaryLevel(E.Salary) AS [Salary Level] FROM Employees AS e
+
+--PROBLEM 06
+CREATE or alter PROCEDURE usp_EmployeesBySalaryLevels(@text VARCHAR(7))
+AS
+     BEGIN
+         SELECT FirstName AS [First Name],
+                LastName AS [Last Name]
+         FROM Employees
+         WHERE dbo.ufn_GetSalaryLevel(Salary) = @text;
+     END;
+
+-- EXEC dbo.usp_EmployeesBySalaryLevels  'low'
+
+
+-- PROBLEM 07
+CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR (50), @word VARCHAR (50)) 
+RETURNS BIT
+AS
+BEGIN
+     DECLARE @index INT = 1
+     DECLARE @letter CHAR(1)
+     WHILE (@index <= LEN(@word))
+     BEGIN
+          SET @letter = SUBSTRING(@word, @index, 1)
+          IF (CHARINDEX(@letter, @setOfLetters) > 0)
+             SET @index = @index + 1
+          ELSE
+             RETURN 0
+     END
+     RETURN 1
+END 
+
+--PROBLEM 08
+CREATE PROCEDURE usp_DeleteEmployeesFromDepartment
+(
+                 @departmentId INT
+)
+AS
+     BEGIN
+         ALTER TABLE Employees ALTER COLUMN ManagerID INT;
+
+         ALTER TABLE Employees ALTER COLUMN DepartmentID INT;
+
+         UPDATE Employees
+           SET
+               DepartmentID = NULL
+         WHERE EmployeeID IN
+         (
+         (
+             SELECT EmployeeID
+             FROM Employees
+             WHERE DepartmentID = @departmentId
+         )
+         );
+
+         UPDATE Employees
+           SET
+               ManagerID = NULL
+         WHERE ManagerID IN
+         (
+         (
+             SELECT EmployeeID
+             FROM Employees
+             WHERE DepartmentID = @departmentId
+         )
+         );
+
+         ALTER TABLE Departments ALTER COLUMN ManagerID INT;
+
+         UPDATE Departments
+           SET
+               ManagerID = NULL
+         WHERE DepartmentID = @departmentId;
+
+         DELETE FROM Departments
+         WHERE DepartmentID = @departmentId;
+
+         DELETE FROM EmployeesProjects
+         WHERE EmployeeID IN
+         (
+         (
+             SELECT EmployeeID
+             FROM Employees
+             WHERE DepartmentID = @departmentId
+         )
+         );
+
+         DELETE FROM Employees
+         WHERE DepartmentID = @departmentId;
+
+         SELECT COUNT(*)
+         FROM Employees
+         WHERE DepartmentID = @departmentId;
+     END;
+
+
+
+
+
