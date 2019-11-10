0b7e3d0229471b3eaad3814eaa961467b48f7882
vencislav <v.verchov@mail.bg>
2019-11-10 23:37:37 +0200

    add Doctor.cs

---
+++ Databases Advanced/Code First/P01_HospitalDatabase/Data/Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Specialty { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; } = new HashSet<Visitation>();
+    }
+}
