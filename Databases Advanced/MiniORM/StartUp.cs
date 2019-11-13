86d21080323678bb9e6d1b99c027a34f33472d5c
vencislav <v.verchov@mail.bg>
2019-11-13 23:07:35 +0200

    add folder MiniORM

---
+++ Databases Advanced/MiniORM/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = @"Server=.\SQLEXPRESS; Database=MiniORM; Integrated Security=True";
+
+            SoftUniDbContextClass context = new SoftUniDbContextClass(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
