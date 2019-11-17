7c46a3d549c7ff561db7e886aecb9e1ecd1902d5
vencislav <v.verchov@mail.bg>
2019-11-17 12:31:16 +0200

    add folder Problem09

---
+++ Databases Advanced/Introduction to DB Apps/Problem09/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using Introduction_to_DB_Apps;
+
+namespace Problem09
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionDatabase))
+            {
+                connection.Open();
+
+                int input = int.Parse(Console.ReadLine());
+
+                string execQuery = @"EXEC usp_GetOlder @id";
+                string print = @"SELECT Name, Age FROM Minions WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(execQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@id", input);
+
+                    command.ExecuteNonQuery();
+                }
+
+                using (SqlCommand command = new SqlCommand(print, connection))
+                {
+                    command.Parameters.AddWithValue("@id", input);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        string name = (string)reader[0];
+                        int age = (int)reader[1];
+
+                        Console.WriteLine($"{name} – {age} years old");
+                    }
+                }
+            }
+        }
+    }
+}
