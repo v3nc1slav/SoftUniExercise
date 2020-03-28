93f1be1e338d7fcd5f6c971514567912d84a6bad
v3nc1slav <v.verchov@mail.bg>
2020-03-28 22:18:44 +0200

    adding the correct file

---
+++ Programming Basics/Simple Operations and Calculations - Lab/08. Fish Tank/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08._Fish_Tank
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine()) * 0.01;
+            double volume = length * width * height * 0.001 * (1 - percentage);
+            Console.WriteLine("{0:F3}", volume);
+        }
+    }
+}
