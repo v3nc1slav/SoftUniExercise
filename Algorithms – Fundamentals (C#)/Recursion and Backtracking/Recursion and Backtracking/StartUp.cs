060fa272e005079a15569c8ae7451b91f6b889b7
v3nc1slav <v.verchov@mail.bg>
2020-11-20 22:58:45 +0200

    add two new problems

---
+++ Algorithms – Fundamentals (C#)/Recursion and Backtracking/Recursion and Backtracking/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace Recursion_and_Backtracking
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(GetSum(array, array.Length-1));
+        }
+
+        private static int GetSum(int[] arr, int index)
+        {
+            if (index == 0)
+            {
+                return arr[index];
+            }
+            return arr[index] + GetSum(arr, index - 1);
+        }
+    }
+}
---
+++ Algorithms – Fundamentals (C#)/Recursion and Backtracking/Recursive Drawing/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace Recursive_Drawing
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Print(input);
+        }
+
+        private static void Print(int input)
+        {
+            if (input==0)
+            {
+                return;
+            }
+            Console.WriteLine(new string ('*', input));
+
+            Print(input - 1);
+
+            Console.WriteLine(new string('#', input));
+        }
+    }
+}
